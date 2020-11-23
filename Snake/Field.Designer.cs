1eacf66d47a719bd663fa00ba4ee3052b6b7f182
イーゴリ <67709368+f55desu@users.noreply.github.com>
2020-11-23 20:35:08 +0300

    Add files via upload

---
+++ Snake/Field.Designer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Snake
+{
+	partial class Field
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private EventHandler Field_Load;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Field));
+			this.Head = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.Head)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Head
+			// 
+			this.Head.Image = ((System.Drawing.Image)(resources.GetObject("Head.Image")));
+			this.Head.Location = new System.Drawing.Point(217, 151);
+			this.Head.Name = "Head";
+			this.Head.Size = new System.Drawing.Size(20, 20);
+			this.Head.TabIndex = 0;
+			this.Head.TabStop = false;
+			// 
+			// Field
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(520, 420);
+			this.Name = "Field";
+			this.Text = "Form1";
+			this.Controls.Add(Head);
+			((System.ComponentModel.ISupportInitialize)(this.Head)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox Head;
+	}
+}
---
+++ Snake/Field.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake
+{
+	public partial class Field : Form
+	{
+        private int _width = 800;
+        private int _height = 800;
+
+		public Field()
+		{
+			InitializeComponent();
+            this.Width = _width;
+            this.Height = _height;
+            this.KeyDown += new KeyEventHandler(OKP);
+		}
+
+        private void OKP(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Right":
+                    Head.Location = new Point(Head.Location.X + 20, Head.Location.Y);
+                    break;
+                case "Left":
+                    Head.Location = new Point(Head.Location.X - 20, Head.Location.Y);
+                    break;
+                case "Up":
+                    Head.Location = new Point(Head.Location.X, Head.Location.Y - 20);
+                    break;
+                case "Down":
+                    Head.Location = new Point(Head.Location.X, Head.Location.Y + 20);
+                    break;
+            }
+
+        }
+	}
+}
